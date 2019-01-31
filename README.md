bb5b34cef0f963421dcdd86ccf72056f04e5059b
Eagle19243 <eagle19243@gmail.com>
2019-01-30 20:14:18 -0500

    remove link from credit, smaller font

---
+++ README.md
@@ -0,0 +1,48 @@
+# Silent Quadrant Password Protection
+
+## What is Silent Quadrant Password Protection?
+
+Silent Quadrant Password Protection is a Chrome extension that you can drop into any web page which dramatically improves the security of users accessing your website.
+
+Silent Quadrant Password Protection works by binding itself to all `input` elements on the page of type
+`email` or `password`. Whenever a user enters a new value into one of these
+`input` elements, Silent Quadrant Password Protection will check the user's email / password against
+the fabulous [Have I Been Pwned?](https://haveibeenpwned.com) API service to see whether or not
+the user's email OR password has been breached in the past.
+
+Finally, if appropriate, Silent Quadrant Password Protectionwill inform the user that their credentials
+were previously breached, give them some useful information, and ask them to
+update their password as soon as they can to reduce the risk that their account
+will be compromised.
+
+Silent Quadrant Password Protection is a new, experimental way to proactively notify users about data
+breaches and help casual web users play a more active role in protecting their
+online accounts.
+
+We believe that by informing and guiding users to reset their credentials when
+necessary, we can all help make the web a safer place &lt;3
+
+
+## How to Use Silent Quadrant Password Protection
+
+1. Download source code.
+2. Open terminal, navigate to the downloaded repository and run commands below:
+    ```
+    npm install
+    npm run build
+    ```
+    After run these commands, you will see `dist` folder generated.
+3. Open `chrome://extensions/` in your Google Chrome, and enable `Developer mode`.
+4. Click `Load unpacked`, and then choose `dist` folder in the presented dialog.
+5. Now you are able to use Silent Quadrant Password Protection Secure Extension.
+
+## Is Silent Quadrant Password Protection Secure?
+
+**YES!**
+
+Silent Quadrant Password Protection never sends or stores password information over the network. It uses
+[k-Anonymity](https://www.troyhunt.com/ive-just-launched-pwned-passwords-version-2/)
+to safely communicate your sensitive data over the internet without risk.
+
+Silent Quadrant Password Protection is also completely free, open source. If you want to audit the source, or have ideas about how to do things better, please open a
+[GitHub issue](https://github.com/Eagle19243/silent-quadrant-chrome/).
---
+++ src/inject.js
@@ -0,0 +1,294 @@
+/**
+ * Detect to see whether or not this script has already been loaded or not. For
+ * instance, if a developer includes passprotect-js in their site and a chrome
+ * extension visitor stumbles across the same site, we don't want to double up
+ * on notifications.
+ */
+var scripts = document.getElementsByTagName("script");
+for (var i = 0; i < scripts.length; i++) {
+  var chunks = scripts[i].src.split("/");
+
+  if (chunks[chunks.length - 1] === "passprotect.min.js") {
+    throw "passprotect already loaded, skipping";
+  }
+}
+
+import * as sha1 from "js-sha1";
+import * as vex from "../vendor/vex.combined.min.js";
+import "../vendor/vex.css";
+import "../vendor/vex-theme-wireframe.css";
+import "./style.css";
+
+
+/**
+ * Settings (for the Vex library)
+ */
+vex.defaultOptions.className = "vex-theme-wireframe";
+vex.defaultOptions.escapeButtonCloses = false;
+vex.defaultOptions.overlayClosesOnClick = false;
+
+/**
+ * Globals
+ */
+var PASS_PROTECT_EMAIL_CHECK_URI = "https://haveibeenpwned.com/api/v2/breachedaccount/";
+var PASS_PROTECT_PASTE_CHECK_URI = "https://haveibeenpwned.com/api/v2/pasteaccountaccount/";
+var PASS_PROTECT_PASSWORD_CHECK_URI = "https://api.pwnedpasswords.com/range/";
+
+
+/**
+ * Format numbers in a nice, human-readable fashion =)
+ *
+ * Stolen from: https://stackoverflow.com/questions/2901102/how-to-print-a-number-with-commas-as-thousands-separators-in-javascript
+ */
+function numberFormatter(number, fractionDigits = 0, thousandSeperator = ',', fractionSeperator = '.') {
+  if (number !==0 && !number || !Number.isFinite(number)) {
+    return number;
+  }
+
+  const frDigits = Number.isFinite(fractionDigits)? Math.min(Math.max(fractionDigits, 0), 7) : 0;
+  const num = number.toFixed(frDigits).toString();
+
+  const parts = num.split('.');
+  let digits = parts[0].split('').reverse();
+  let sign = '';
+
+  if (num < 0) {
+    sign = digits.pop();
+  }
+
+  let final = [];
+  let pos = 0;
+
+  while (digits.length > 1) {
+      final.push(digits.shift());
+      pos++;
+
+      if (pos % 3 === 0) {
+        final.push(thousandSeperator);
+      }
+  }
+
+  final.push(digits.shift());
+  return `${sign}${final.reverse().join('')}${frDigits > 0 ? fractionSeperator : ''}${frDigits > 0 && parts[1] ? parts[1] : ''}`;
+}
+
+
+/**
+ * This function returns true if the data is ignored and should not be used to
+ * fire off a notification, false otherwise.
+ *
+ * @param {string} sensitiveData - The sensitive data to check for in
+ *      localStorage / sessionStorage.
+ */
+function isIgnored(sensitiveData) {
+  var data = sessionStorage.getItem(sensitiveData) || localStorage.getItem(sensitiveData);
+
+  return data === "true" ? true : false;
+}
+
+
+/**
+ * This function binds our protection to any suitable input elements on the
+ * page. This way, we'll fire off the appropriate checks when an input value
+ * changes.
+ */
+function protectInputs() {
+  var inputs = document.getElementsByTagName("input");
+
+  for (var i = 0; i < inputs.length; i++) {
+    switch (inputs[i].type) {
+      case "email":
+        //inputs[i].addEventListener("change", protectEmailInput);
+        break;
+      case "password":
+        inputs[i].addEventListener("change", protectPasswordInput);
+        break;
+    }
+  }
+
+  //inputs = document.querySelectorAll("input[type='text']");
+  //for (var i = 0; i < inputs.length; i++) {
+  //  if (inputs[i].name.toLowerCase().indexOf("email") !== -1) {
+  //    return inputs[i].addEventListener("change", protectEmailInput);
+  //  }
+
+  //  if (inputs[i].id.toLowerCase().indexOf("email") !== -1) {
+  //    return inputs[i].addEventListener("change", protectEmailInput);
+  //  }
+
+  //  if (inputs[i].placeholder.toLowerCase().indexOf("email") !== -1) {
+  //    return inputs[i].addEventListener("change", protectEmailInput);
+  //  }
+  //}
+}
+
+
+/**
+ * Return a unique email hash suitable for caching.
+ *
+ * @param {string} email - The email address to hash.
+ */
+function getEmailHash(email) {
+  return sha1(email + "-" + getHost());
+}
+
+
+/**
+ * Return the top level host name for a domain. EG: Given woot.adobe.com, will
+ * return adobe.com.
+ */
+function getHost() {
+  return window.location.host.split('.').slice(-2).join('.');
+}
+
+
+/**
+ * Protect email input elements. When a value is entered, we'll check the email
+ * address against the haveibeenpwned API, then warn the user if their
+ * credentials were compromised on the site they're currently on.
+ *
+ * @param {object} evt - The DOM event.
+ */
+function protectEmailInput(evt) {
+  var host = getHost();
+  var xmlHttp = new XMLHttpRequest();
+  var inputValue = evt.currentTarget.value;
+  var badEmailDetected = false;
+
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+      var breaches = JSON.parse(xmlHttp.responseText);
+
+      for (var i = 0; i < breaches.length; i++) {
+        if (breaches[i].Domain === host && breaches[i].IsVerified) {
+          badEmailDetected = true;
+          var message = [
+            '<p>' + breaches[i].Description + '</p>',
+            '<p>The email you entered was one of the <b>' + numberFormatter(breaches[i].PwnCount) + "</b> that were compromised. If you haven't done so already, you should change your password.</p>"
+          ].join('');
+
+          vex.dialog.alert({
+            message: "Breach detected!",
+            input: message,
+            callback: function() {
+              // Cache this email once the user clicks the "I Understand" button
+              // so we don't continuously annoy the user with the same warnings.
+              localStorage.setItem(getEmailHash(inputValue), "true");
+            }
+          });
+        }
+      }
+    };
+  };
+
+  // If this email is cached, we shouldn't do anything.
+  if (isIgnored(getEmailHash(inputValue))) {
+    return;
+  }
+
+  xmlHttp.open("GET", PASS_PROTECT_EMAIL_CHECK_URI + encodeURIComponent(inputValue), true);
+  xmlHttp.send(null);
+}
+
+
+/**
+ * Generate a unique hash which we can store locally to remember a password.
+ * Now, it would obviously be unsafe to store a password hash in sessionStorage
+ * (because if an XSS occurs it could be bad).
+ *
+ * BUT, what we can do to reduce risk and still maintain SOME sort of knowledge
+ * (albeit, with a fairly high collision risk), we can essentially compute the
+ * password hash, grab just the first 5 chars of it, then tack on our host info,
+ * then hash the resulting string.
+ *
+ * This way we can reassemble a tiny bit of what we've got without potentially
+ * leaking sensitive information.
+ *
+ * @param {string} password - The password to hash.
+ */
+function getPasswordHash(password) {
+  return sha1(sha1(password).slice(0, 5) + "-" + getHost());
+}
+
+
+/**
+ * This function runs whenever a password input's value changes. It protects
+ * checks the password against the haveibeenpwned API and alerts the user if the
+ * password they've entered has been breached.
+ *
+ * @param {object} evt - The DOM event object.
+ */
+function protectPasswordInput(evt) {
+  var inputValue = evt.currentTarget.value;
+  var hash = sha1(inputValue).toUpperCase();
+  var hashPrefix = hash.slice(0, 5);
+  var shortHash = hash.slice(5);
+  var xmlHttp = new XMLHttpRequest();
+
+  xmlHttp.onreadystatechange = function() {
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+      var resp = xmlHttp.responseText.split("\n");
+
+      for (var i = 0; i < resp.length; i++) {
+        var data = resp[i].split(":");
+
+        if (data[0].indexOf(shortHash) === 0) {
+          var message = [
+            '<dic class="silent-quadrant-dialog-content">',
+            '<img src="' + chrome.extension.getURL('images/logo.png') + '" alt="Silent Quadrant Logo" class="silent-quadrant-logo1" />',
+            '<div class="silent-quadrant-title">Unsafe Password Detected</div>',
+            '<p>The password you entered has been found in ' + numberFormatter(parseInt(data[1]))  + ' data breaches and is not safe to use.</p>',
+            '<p>A password that has been involved in a data breach is easily accessible to attackers. Please change this password wherever you use it so it can not be leveraged to gain access to your account(s).</p>',
+            '<p>This notice will not display again for the duration of this session.</p>',
+            '<button class="silent-quadrant-dismiss">I Understand</button>',
+            '<img src="' + chrome.extension.getURL('images/spyglass.png') + '" alt="Logo" class="silent-quadrant-logo2" />',
+            '<div class="silent-quadrant-credit">credit: haveibeenpwned.com</div>',
+            '</div>'
+          ].join('');
+
+          vex.dialog.alert({
+            unsafeMessage: message,
+            callback: function() {
+              // Cache this password once the user clicks the "I Understand" button
+              // so we don't continuously annoy the user with the same warnings.
+              //
+              // NOTE: We're using sessionStorage here (not localStorage) as we
+              // only want to not annoy the user for the duration of this
+              // session. Once they've come back to the site at a later time, we
+              // should bug them if they try to use the same password >:D
+              sessionStorage.setItem(getPasswordHash(inputValue), "true");
+            }
+          });
+        }
+      }
+    }
+  };
+
+  // If this hash is cached, we shouldn't do anything.
+  if (isIgnored(getPasswordHash(inputValue))) {
+    return;
+  }
+
+  // We're using the API with k-Anonymity searches to protect privacy.
+  // You can read more about this here: https://haveibeenpwned.com/API/v2#SearchingPwnedPasswordsByRange
+  xmlHttp.open("GET", PASS_PROTECT_PASSWORD_CHECK_URI + hashPrefix, true);
+  xmlHttp.send(null);
+}
+
+
+// Bootstrap our passProtect functionality after the page has fully loaded.
+if (window.attachEvent) {
+  window.attachEvent("onload", protectInputs);
+} else {
+  if (window.onload) {
+    var currentOnLoad = window.onload;
+    var newOnLoad = function(evt) {
+      currentOnLoad(evt);
+      protectInputs(evt);
+    };
+
+    window.onload = newOnLoad;
+  } else {
+    window.onload = protectInputs;
+  }
+}
